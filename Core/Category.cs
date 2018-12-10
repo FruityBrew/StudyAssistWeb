47edd75d8cbd23ab9c0764b81d938aad7e267c62
FruityBrew <kompota@yandex.ru>
2018-12-10 13:16:04 +0300

    #5 классы модели

---
+++ Core/Category.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StudyAssist.Core.Interfaces;
+
+namespace StudyAssist.Core
+{
+    public class Category : ICategory
+    {
+        #region Fields
+
+        String _name;
+
+        private List<ITheme> _themes;
+
+        #endregion
+
+        #region ctors
+
+        public Category()
+        {
+            _themes = new List<ITheme>();
+        }
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Название категории.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException(
+                        "Название категории не может быть пустым");
+
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Список тем. //TODO переделать на список?
+        /// </summary>
+        public IEnumerable<ITheme> Themes
+        {
+            get { return _themes; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public void RemoveFromStudy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTheme(ITheme addedItem)
+        {
+            _themes?.Add(addedItem);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Core/Theme.cs
@@ -0,0 +1,97 @@
+﻿using StudyAssist.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudyAssist.Core
+{
+    public class Theme : ITheme
+    {
+        #region fields   
+
+        private String _name;
+
+        private Boolean _isStudy;
+
+        private List<IProblem> _problems;
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Показывает находится ли тема на изучении.
+        /// </summary>
+        public bool IsStudy
+        {
+            get { return _isStudy; }
+            set { _isStudy = value; }
+        }
+
+        /// <summary>
+        /// Название темы.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException(
+                        "Название темы на может быть пустым");
+                else
+                    _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Коллекция проблем.
+        /// </summary>
+        public IEnumerable<IProblem> Problems
+        {
+            get { return _problems; }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Theme()
+        {
+            _problems = new List<IProblem>();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Удаляет тему и все проблемы темы. 
+        /// </summary>
+        public void RemoveFromStudy()
+        {
+            IsStudy = false;
+
+            if (_problems == null)
+                return;
+
+            Parallel.ForEach(_problems, (a) => a.IsStudy = false);
+        }
+
+        /// <summary>
+        /// Добавляет проблему к списку.
+        /// </summary>
+        /// <param name="addedItem"></param>
+        public void AddProblem(IProblem addedItem)
+        {
+            if(_problems != null)
+                _problems.Add(addedItem);
+        }
+
+        #endregion Methods
+    }
+}
