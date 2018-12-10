a3dbf92a432d73284869c7a99df9bb03f0896748
FruityBrew <kompota@yandex.ru>
2018-12-10 12:59:56 +0300

    #5 Интерфейсы

---
+++ CoreInterfaces/ICategory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyAssist.Core.Interfaces
+{
+    /// <summary>
+    /// Категория - контейнер для тем.
+    /// </summary>
+    public interface ICategory
+    {
+        #region Properties
+
+        /// <summary>
+        /// Имя категории.
+        /// </summary>
+        String Name { get; set; }
+
+        /// <summary>
+        /// Коллекция тем.
+        /// </summary>
+        IEnumerable<ITheme> Themes { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Удаляет категорию с обучения.
+        /// </summary>
+        void RemoveFromStudy();
+
+        /// <summary>
+        /// Добавляет тему в коллекцию тем.
+        /// </summary>
+        /// <param name="addedItem">Добавляемая тема.</param>
+        void AddTheme(ITheme addedItem);
+
+        #endregion Methods
+    }
+}
---
+++ CoreInterfaces/ITheme.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudyAssist.Core.Interfaces
+{
+    /// <summary>
+    /// Интерфейс темы (подкатегории).
+    /// </summary>
+    public interface ITheme
+    {
+        #region Properties
+
+        /// <summary>
+        /// Название темы.
+        /// </summary>
+        String Name { get; set; }
+
+        /// <summary>
+        /// Признак того, находится ли тема на изучении.
+        /// </summary>
+        Boolean IsStudy { get; set; }
+
+        /// <summary>
+        /// Коллекция вопросов темы.
+        /// </summary>
+        IEnumerable<IProblem> Problems { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Удаляет тему с обучения.
+        /// </summary>
+        void RemoveFromStudy();
+
+        /// <summary>
+        /// Добавляет проблему в коллекцию проблем.
+        /// </summary>
+        /// <param name="addedItem"></param>
+        void AddProblem(IProblem addedItem);
+
+        #endregion Methods
+    }
+}
