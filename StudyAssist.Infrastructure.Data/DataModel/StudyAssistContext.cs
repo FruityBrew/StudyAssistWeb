a9d89890173872bc8cc60b9bdc137404a3641c4d
FruityBrew <kompota@yandex.ru>
2018-12-11 13:49:42 +0300

    #6 Init mgr

--- StudyAssist.Infrastructure.Data/DataModel/StudyAssistContext.cs
+++ StudyAssist.Infrastructure.Data/DataModel/StudyAssistContext.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace StudyAssist.Infrastructure.Data.DataModel
 {
@@ -24,7 +26,17 @@ namespace StudyAssist.Infrastructure.Data.DataModel
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StudyAssist_1;Trusted_Connection=True;");
+            //var configBuilder = new ConfigurationBuilder();
+
+            //var v = Directory.GetCurrentDirectory();
+
+            //configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            //configBuilder.AddJsonFile("appsettings.json");
+            //var config = configBuilder.Build();
+            //string connString = config.GetConnectionString("Def");
+            string connString = "Server=tcp:tokeyserver.database.windows.net,1433;Initial Catalog=StudyAssistDb;Persist Security Info=False;User ID=fruitybrew;Password=k19061906!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            builder.UseSqlServer(connString);
         }
 
         #endregion MethodOverrides
---
+++ StudyAssist.Infrastructure.Data/Migrations/20181211095746_init.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudyAssist.Infrastructure.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Themes",
+                columns: table => new
+                {
+                    IsStudy = table.Column<bool>(nullable: false),
+                    ThemeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: true),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Themes", x => x.ThemeId);
+                    table.ForeignKey(
+                        name: "FK_Themes_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Problems",
+                columns: table => new
+                {
+                    ProblemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Question = table.Column<string>(maxLength: 200, nullable: false),
+                    Answer = table.Column<string>(nullable: true),
+                    RepeatDate = table.Column<DateTime>(nullable: true),
+                    IsAutoRepeate = table.Column<bool>(nullable: false),
+                    IsStudy = table.Column<bool>(nullable: false),
+                    ThemeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problems", x => x.ProblemId);
+                    table.ForeignKey(
+                        name: "FK_Problems_Themes_ThemeId",
+                        column: x => x.ThemeId,
+                        principalTable: "Themes",
+                        principalColumn: "ThemeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Problems_ThemeId",
+                table: "Problems",
+                column: "ThemeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Themes_CategoryId",
+                table: "Themes",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Problems");
+
+            migrationBuilder.DropTable(
+                name: "Themes");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
