0ef8b8f020edc9edd61130ea3b4276d536fb87a9
FruityBrew <kompota@yandex.ru>
2021-03-17 15:29:57 +0300

    #model Добавил в модель изучаемый вопрос и пользователя

---
+++ StudyAssistModel/DataModel/IssueUnderStudy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudyAssistModel.DataModel
+{
+    public class IssueUnderStudy
+    {
+        public int IssueUnderStudyId { get; set; }
+
+        //public Issue Issue { get; set; }
+        public int IssueId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime? RepeateDate { get; set; }
+
+        public int StudyLevel { get; set; }
+    }
+}
