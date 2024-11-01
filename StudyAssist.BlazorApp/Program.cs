d0e4081bfb4327dbfd642808e8327d42bc5042d7
FruityBrew <kompota@yandex.ru>
2024-11-01 15:21:50 +0300

    #blazor добавил блейзор приложение для клиента + бутстрап

---
+++ StudyAssist.BlazorApp/Program.cs
@@ -0,0 +1,32 @@
+using StudyAssist.BlazorApp.Components;
+
+namespace StudyAssist.BlazorApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+			//builder.Services.AddBlazorBootstrap();
+
+
+			var app = builder.Build();
+			
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.UseStaticFiles();
+			
+			//app.UseRouting();
+			//app.MapGet("/", () => "Hello World!");	
+
+
+			app.Run();
+		}
+	}
+}
