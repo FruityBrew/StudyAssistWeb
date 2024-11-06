3d7eab1d954b39b9ab3542ed517c6cf0666435a5
FruityBrew <kompota@yandex.ru>
2024-11-06 17:40:10 +0300

    #appApi dto

---
+++ StudyAssist.App.Api/Dtos/CatalogsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace StudyAssist.App.Api.Dtos
+{
+    public class CatalogsDto
+    {
+        public CatalogsDto() 
+        {
+            Catalogs = new();
+            Themes = new();
+            Issues = new();
+        }
+
+        public Dictionary<int, string> Catalogs { get; set; }
+
+        public Dictionary<int, (int CatalogId, string Name)> Themes { get; set; }
+
+        public Dictionary<int, (int CatalogId, int ThemeId, string Name)> Issues { get; set; }
+    }
+}
