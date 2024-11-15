63b76a9e72566046ec58f2f7384217845b479647
FruityBrew <kompota@yandex.ru>
2024-11-15 21:12:04 +0300

    #converter накидал код для сохранения каталога

--- StudyAssist.KnowledgeDataConverter/KnowledgeDataProvider.cs
+++ StudyAssist.KnowledgeDataConverter/KnowledgeDataProvider.cs
@@ -1,18 +1,56 @@
-﻿using StudyAssist.Model;
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
+using StudyAssist.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
 using Utilities.Interfaces;
+using static IdentityModel.OidcConstants;
 
 namespace StudyAssist.KnowledgeDataConverter
 {
     internal class KnowledgeDataProvider : IKnowledgeDataProvider
     {
-        public Task<int> SaveCatalog(Catalog catalog)
+        private readonly IHttpClientFactory _httpClientFactory;
+        //private readonly IConfiguration _configuration;
+
+        private string _identityServerUri = @"AuthConfig:IdentityServerAuthorityUrl";
+
+        private string _dataAccessUri = @"WebApiUrls:KnowledgeDataAccessApi";
+
+        public async Task<int> SaveCatalog(Catalog catalog)
         {
-            throw new NotImplementedException();
+            //HttpClient identityServerClient = _httpClientFactory.CreateClient();
+
+            //DiscoveryDocumentResponse discoveryDocument = await identityServerClient.GetDiscoveryDocumentAsync(
+            //    _identityServerUri);
+
+            //var tokenResponse = await identityServerClient.RequestClientCredentialsTokenAsync(
+            //    new ClientCredentialsTokenRequest
+            //    {
+            //        Address = discoveryDocument.TokenEndpoint,
+            //        ClientId = "studyAssistConverter_id",
+            //        ClientSecret = "studyAssistConverter_secret",
+            //        GrantType = GrantTypes.ClientCredentials,
+            //        Scope = "User" // todo
+
+            //    });
+
+            HttpClient dataAccessClient = _httpClientFactory.CreateClient();
+            //dataAccessClient.SetBearerToken(tokenResponse.AccessToken);
+
+            HttpContent content = JsonContent.Create(catalog);
+
+
+            var response = await dataAccessClient.PostAsync(
+                _dataAccessUri, content);
+
+            return 1;
         }
     }
 }
