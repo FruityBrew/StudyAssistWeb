5a3199e7fbf1323435f84001803565422bfd2aa9
FruityBrew <kompota@yandex.ru>
2024-11-21 10:21:06 +0300

    #dataApi убрал проверку обязательного Answer

--- KnowledgeDataAccessApi/Validators/IssueValidator.cs
+++ KnowledgeDataAccessApi/Validators/IssueValidator.cs
@@ -17,7 +17,7 @@ namespace KnowledgeDataAccessApi.Validators
                 .Must(issue => issue.Length <= 200)
                 .WithMessage(MessageTemplates.QUESTION_TEXT_CONSTRAINT_ERR);
 
-            RuleFor(issue => issue.Answer).NotNull().NotEmpty();
+            //RuleFor(issue => issue.Answer).NotNull().NotEmpty();
         }
     }
 }
